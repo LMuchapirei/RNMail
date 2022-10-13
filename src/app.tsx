e44a43cff257d359bbd69dd6fbeccd6780f07a30
lmuchapirei <h170407h@hit.ac.zw>
2022-10-13 19:36:18 +0200

    Add new structure a bit and show hello world

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import {View,Text} from 'react-native'
+
+
+const App=()=>{
+    return (
+        <NavigationContainer>
+            <View>
+            <Text>Hello, World</Text>
+            </View>
+        </NavigationContainer>
+    )
+}
+
+export default App
